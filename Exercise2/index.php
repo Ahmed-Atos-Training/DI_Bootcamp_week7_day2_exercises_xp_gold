f67a3ea44f00691e647e1c95c58dc5fefb9cc171
Lenikpoho Ahmed KONE <47745603+koneahmed@users.noreply.github.com>
2023-02-10 14:07:30 +0100

    update

--- Exercise2/index.php
+++ Exercise2/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>exercice2</title>
 </head>
+
 <body>
     <form action="" method="post">
         <h2>Calcul Number</h2>
@@ -19,17 +21,18 @@
         </div>
         <input type="submit" name="submit" value="valider">
     </form>
-    
-    <?php 
+
+    <?php
     //le script permetant de calculer la somme des nombres entrer par l'utilisateur
 
-        if (isset($_POST['submit']) && $_POST['submit']) {
+    if (isset($_POST['submit']) && $_POST['submit']) {
 
-            $somme = $_POST['nombre1'] + $_POST['nombre2'];
-            echo "la somme est: " .$somme;
-        }
+        $somme = $_POST['nombre1'] + $_POST['nombre2'];
+        echo "la somme est: " . $somme;
+    }
 
     ?>
-    
+
 </body>
+
 </html>
